70c508b66a0336ba4286082af197e5fe18d95d21
Will Zhao <will@publica.co.nz>
2023-04-13 20:00:05 +1200

    format data. Add extra filed for display.

--- src/views/students/StudentList.vue
+++ src/views/students/StudentList.vue
@@ -2,11 +2,11 @@
     <div>
         <el-table :data="data.tableData" border style="width: 100%">
             <el-table-column prop="name" label="Name" align="center" />
-            <el-table-column prop="sex" label="Gender" align="center" />
+            <el-table-column prop="sex_text" label="Gender" align="center" />
             <el-table-column prop="age" label="Age" align="center" />
             <el-table-column prop="number" label="Number" align="center" />
             <el-table-column prop="class" label="Class" align="center" />
-            <el-table-column prop="state" label="State" align="center" />
+            <el-table-column prop="state_text" label="State" align="center" />
             <el-table-column prop="address" label="Address" align="center" />
             <el-table-column prop="phone" label="Phone" align="center" />
             <el-table-column label="Operation">
@@ -29,6 +29,14 @@ onMounted(()=>{
         console.log(res)
         if(res.data.status===200) {
             data.tableData = res.data.data
+            data.tableData.forEach(item => {
+                item.sex === 1 ? item.sex_text = 'Male' : item.sex_text = 'Female'
+                item.status === 1
+                ? (item.state_test = 'Enrolled')
+                : item.state == '2'
+                ? (item.state_text = 'Not Enroled')
+                : (item.state_text = 'Reset')
+            })
         }
     })
 })
