6e6ba9d4a010dcccf2a7ad3bc9a2597faa0d7b36
Will Zhao <will@publica.co.nz>
2023-04-15 09:15:22 +1200

    add style on map.

--- src/views/dataAnalysis/MapView.vue
+++ src/views/dataAnalysis/MapView.vue
@@ -15,10 +15,18 @@ onMounted(()=> {
     echarts.registerMap('china', geoJson)
 
     let option = {
+        backgroundColor: 'rgb(121, 145, 209)',
         geo: {
             map: 'china',
             aspectScale: 0.75,
             zoom: 1.1,
+            // itemStyle: {
+            //     normal: {
+            //         shadowColor: 'rgb(58, 115, 192)',
+            //         shadowOffsetX: 10,
+            //         shadowOffsetY: 11,
+            //     }
+            // },
             regions: [{
                 name: 'South Island'
             }]
@@ -26,8 +34,42 @@ onMounted(()=> {
         series: [
             {
                 type: 'map',
+                label: {
+                    normal: {
+                        show: true,  //show name
+                        textStyle: {
+                            color: '#1DE'
+                        }
+                    },
+                },
                 zoom: 1.1,
-                map: 'china'
+                map: 'china',
+                itemStyle: {
+                    normal: {
+                        backgroundColor: 'rgb(147, 235, 248)',
+                        areaColor: {
+                            type: 'radial',
+                            x: 0.5,
+                            y: 0.5,
+                            r: 0.8,
+                            colorStops: [
+                                {
+                                    offset: 0,
+                                    color: 'rgb(31, 54, 150)'
+                                },
+                                {
+                                    offset: 1,
+                                    color: 'rgb(89, 128, 142)'
+                                }
+                            ],
+                            globalCoord: true
+                        }
+                    },
+                    emphasis: {
+                        areaColor: 'rgb(46, 229, 206)',
+                        borderWidth: 0.1
+                    }
+                }
             }
         ]
     }
